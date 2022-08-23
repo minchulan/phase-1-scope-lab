dbed17f80f3e3136e4a27770148f98e8aa0752f2
Minchul An <min@minchulan.io>
2022-08-23 11:38:28 -0400

    commit tree ad20cacf6ad45f21c1e33ed30d232847651a53ec

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your solution in this file!
+const customerName = "bob";
+
+function UpperCaseCustomerName() {
+  console.log(customerName.toUpperCase);
+  function setBestCustomer() {
+    const bestCustomer = "not bob";
+  }
+}
